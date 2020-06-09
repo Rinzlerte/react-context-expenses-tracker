951735b764c75762e51691a2f1f333e4aa22cf44
Serg Nazarchuk <tesejz@gmail.com>
2020-06-09 14:02:17 +0300

    v

--- src/context/AppReducer.js
+++ src/context/AppReducer.js
@@ -9,13 +9,11 @@ export default (state, action) => {
           ),
         };
 
-    //   case "ADD TRANSACTION":
-    //     return {
-    //       ...state,
-    //       transactions: state.transactions.filter(
-    //         (item) => item.id != action.payload
-    //       ),
-    //     };
+      case "ADD TRANSACTION":
+        return {
+          ...state,
+          transactions: [action.payload, ...state.transactions ]
+        };
 
       default:
         return state;
--- src/context/GlobalState.js
+++ src/context/GlobalState.js
@@ -1,15 +1,14 @@
 import React, { createContext , useReducer} from 'react'
 
 import AppReducer from './AppReducer'
-
 // inithiating globalstate
 const InitialState = {
   transactions: [
-    { id: 1, text: "Flower", amount: -20 },
-    { id: 2, text: "Salary", amount: 300 },
-    { id: 3, text: "Book", amount: -10 },
-    { id: 4, text: "Camera", amount: 150 },
-    { id: 5, text: "Cam", amount: 100 },
+    // { id: 1, text: "Flower", amount: -20 },
+    // { id: 2, text: "Salary", amount: 300 },
+    // { id: 3, text: "Book", amount: -10 },
+    // { id: 4, text: "Camera", amount: 150 },
+    // { id: 5, text: "Cam", amount: 100 },
   ],
 };
 
@@ -35,17 +34,20 @@ export const GlobalProvaider = ({children}) => {
         dispatch({
           type: "ADD TRANSACTION",
           // type is always text
-          payload: id,
+          payload: transaction,
           // paylad is always data to send
         });
     }
 
-     return (
-         <GlobalContext.Provider value={{
-             transactions : state.transactions,
-             deleteTransaction
-         }}>
-             {children}
-         </GlobalContext.Provider>
-     )
+    return (
+      <GlobalContext.Provider
+        value={{
+          transactions: state.transactions,
+          deleteTransaction,
+          addTransaction,
+        }}
+      >
+        {children}
+      </GlobalContext.Provider>
+    );
 }
